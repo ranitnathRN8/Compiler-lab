760d64bac4855ddf5d27c95cd390179fc75a2251
Ranit Nath <42543785+ranitnathRN8@users.noreply.github.com>
2020-02-26 14:38:01 +0530

    Add files via upload

---
+++ Compiler lab/EXP 8/client.c
@@ -0,0 +1,11 @@
+//Initial decleration
+#include<stdio.h>
+
+/*The main program
+to calculate the square of a number */
+int main(){
+	int n;
+	printf("Enter the number \n");
+	scanf("%d",&n);
+	printf("its square is: %d\n",n*n);
+}
